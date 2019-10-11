3408a175114a43a5b819d76a15d3a1dab5b50e20
xujinqiang <xujinqiang@hgandin.com>
2019-10-11 11:40:31 +0800

    add

--- src/main/java/com/andin/controller/TestController.java
+++ src/main/java/com/andin/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.andin.controller;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Part;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,15 +36,39 @@ public class TestController {
 			String app = redisUtil.get("name");
 			map.put("data", app);
 			map.put("resultMsg", "请求成功");
-			map.put("resultStatus", "0000");
+			map.put("resultCode", "0000");
 			logger.debug("TestController.getAppInfo method execute is successful...");
 		} catch (Exception e) {
 			map.put("resultMsg", "请求失败");
-			map.put("resultStatus", "0001");
+			map.put("resultCode", "0001");
 			logger.error("TestController.getAppInfo method execute is error: ", e.getMessage());
 		}
 		return map;
 	}
 	
+	@RequestMapping("/file")
+	@ResponseBody
+	public Map<String, Object> getAppInfo(@RequestParam("file") Part part){
+		logger.debug("TestController.getAppInfo method execute is start...");
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			InputStream in = part.getInputStream();
+			FileOutputStream fos = new FileOutputStream("D://jj.eml");
+			int len = 0;
+			byte[] b = new byte[1024];
+			while ((len = in.read(b)) != -1) {
+				fos.write(b, 0, len);
+			}
+			fos.close();
+			map.put("resultMsg", "请求成功");
+			map.put("resultCode", "0000");
+			logger.debug("TestController.getAppInfo method execute is successful...");
+		} catch (Exception e) {
+			map.put("resultMsg", "请求失败");
+			map.put("resultCode", "0001");
+			logger.error("TestController.getAppInfo method execute is error: ", e.getMessage());
+		}
+		return map;
+	}
 	
 }
